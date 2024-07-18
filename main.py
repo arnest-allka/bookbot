5ea495ca3d82fb155a6db4bb7642ca357b9c4862
Arnest Allka <arisallkas@gmail.com>
2024-07-18 17:07:39 +0300

    print a nice looking report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def countWords(file_contents):
     words = file_contents.split()
-    print(len(words))
+    return len(words)
 
 def countChars(file_contents):
     lowered_char = file_contents.lower()
@@ -12,14 +12,31 @@ def countChars(file_contents):
         else:
             char_count[char] = 1
 
-    print(char_count)
+    return char_count
     
+def reportBookChars(words,dict):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words} words found in the document\n")
+    
+    keys = list(dict.keys())
+    keys.sort()
+    sorted_dict = {i: dict[i] for i in keys}
+    
+    for key, value in sorted_dict.items():
+        if key.isalpha():
+            print(f"The {key} character was found {value} times")
+            
+    print("--- End report ---")
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    countWords(file_contents)
+    # print(countWords(file_contents))
 
-    countChars(file_contents)
+    # print(countChars(file_contents))
+    
+    reportBookChars(countWords(file_contents),countChars(file_contents))
+    
 
 main()
