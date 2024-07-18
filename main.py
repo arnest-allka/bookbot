ac1673e1967b1f4057c52e9239a254ba74e63c87
Arnest Allka <arisallkas@gmail.com>
2024-07-18 16:35:14 +0300

    created func for counting words of a book

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def countWords(file_contents):
+    words = file_contents.split()
+    print(len(words))
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+
+    countWords(file_contents)
 
 main()
